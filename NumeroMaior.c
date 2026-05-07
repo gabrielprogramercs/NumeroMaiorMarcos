c7e31a5c1063a9312204aa32c099a820b9582681
Gabriel Lopes <gabriel-fernando2008@hotmail.com>
2026-05-07 20:08:40 -0300

    Update NumeroMaior.c

--- NumeroMaior.c
+++ NumeroMaior.c
@@ -9,16 +9,29 @@ int main() {
 
     printf("Digite o segundo numero: ");
     scanf("%d", &num2);
-
+    if(num1 > num2){
     switch (num1 > num2)
     {
     case 1:
         printf("O maior numero e o primeiro: %d\n", num1);
         break;
         
+        
 
     default: printf("O maior numero e o segundo: %d\n", num2);
         break;
+    }
+    }
+    else{
+    switch (num1 == num2)
+    {
+    case 1:
+        printf("Os numeros sao iguais: %d\n", num1);
+        break;
+    
+    default:
+        break;
+    }
     }
 
     return 0;
